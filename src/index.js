52664daead962334866ab1c035297154aa4ea103
Tim Garrity <tag.garrity@gmail.com>
2018-11-01 00:52:30 -0600

    apply redux imports and thunks to index.js

--- src/index.js
+++ src/index.js
@@ -10,11 +10,14 @@ import { Provider } from 'react-redux'
 import rootReducer from './reducers'
 import { createStore, applyMiddleware } from 'redux'
 
+const store = create(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, document.getElementById('root'));
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
