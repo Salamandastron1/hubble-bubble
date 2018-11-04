1e1a18b71cccab47d3ede6456c6801d50b4b9224
Tim Garrity <tag.garrity@gmail.com>
2018-11-04 16:05:51 -0700

    implement individual pages for each star card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 const Card = ({star}) => {
-  const { name, image_files, description } = star
+  const { name, image_files, description, id } = star
   return (
-    <article className='card'>
-      <h4>{name}</h4>
-      <img src={image_files} height='400' width='500'/>
-      <p>{description}</p>
-    </article>
+    <Link to={`/studytime/${id}`}>
+      <article className='card'>
+        <h4>{name}</h4>
+        <img src={image_files} height='400' width='500'/>
+      </article>
+    </Link>
   )
 }
 
