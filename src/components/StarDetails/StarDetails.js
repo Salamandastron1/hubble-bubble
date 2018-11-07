77ddc4fe8fd79c3e1711f239fac6a8fb65108f85
Tim Garrity <tag.garrity@gmail.com>
2018-11-06 19:50:37 -0700

    style individual cards

--- src/components/StarDetails/StarDetails.js
+++ src/components/StarDetails/StarDetails.js
@@ -3,13 +3,31 @@ import './StarDetails.css'
 import { Link } from 'react-router-dom'
 
 const StarDetails = (props) => {
-  const {image_files, description, name} = props
+  const { image_files, description, name } = props
   return(
-    <section>
-      <Link to='/studytime' className='back-btn'>◀ back</Link>
-      <h2>{name}</h2>
-      <img src={image_files} alt ={name}/>
-      <p>{description} </p>
+    <section className='individual-card'>
+      <Link 
+        to='/studytime' 
+        className='back-btn'
+      >
+        ◀ back
+      </Link>
+      <h2
+        className='individual-name'
+      >
+        {name}
+      </h2>
+      <img 
+        src={image_files} 
+        alt ={name}
+        height='500'
+        width='500'
+      />
+      <p
+        className='individual-description'
+      >
+        {description} 
+      </p>
     </section>
   )
 }
